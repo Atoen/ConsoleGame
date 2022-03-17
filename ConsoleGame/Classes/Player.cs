6faa6647e4b92905c595fd810334467b449209b1
Artur B <bachtaartur02@gmail.com>
2022-03-17 20:53:02 +0100

    zam

--- ConsoleGame/Classes/Player.cs
+++ ConsoleGame/Classes/Player.cs
@@ -1,6 +1,33 @@
-﻿namespace ConsoleGame.Classes;
+﻿using ConsoleGame.Structs;
+
+namespace ConsoleGame.Classes;
 
 public class Player
 {
+    private static readonly char[,] Sprite =
+    {
+        {' ', '^', ' '},
+        {'/', '■', '\\'},
+        {'|', ' ', '|'}
+    };
+
+    private const ConsoleColor Color = ConsoleColor.DarkGreen;
+    private const ConsoleColor ProjectileColor = ConsoleColor.Cyan;
+    private const int AttackDelay = 5;
     
+    // public Position Pos { get; }
+    // public int Health { get; }
+
+    public Position Pos;
+    public int Health = 5;
+        
+    private int _attackCD;
+
+    public Player(int startX = 30, int startY = 20)
+    {
+        Pos.X = startX;
+        Pos.Y = startY;
+    }
+
+
 }
---
+++ ConsoleGame/Structs/Position.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleGame.Structs;
+
+public struct Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+    public bool Equals(Position other)
+    {
+        return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Position other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public static bool operator ==(Position left, Position right)
+    {
+        return left.X == right.X && left.Y == right.Y;
+    }
+
+    public static bool operator !=(Position left, Position right)
+    {
+        return !(left == right);
+    }
+}

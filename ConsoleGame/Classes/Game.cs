930aaefb0da6ddd4709facffdab04bf842de24e8
Artur B <bachtaartur02@gmail.com>
2022-03-17 20:06:05 +0100

    gamer

---
+++ ConsoleGame/Classes/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleGame.Classes;
+
+public class Game
+{
+    public Game()
+    {
+        
+    }
+
+    #pragma warning disable CA1416
+    public void Start()
+    {
+        Console.WindowWidth = 80;
+        Console.WindowHeight = 30;
+        
+        Console.SetBufferSize(80, 30);
+    }
+}
---
+++ ConsoleGame/Classes/Player.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleGame.Classes;
+
+public class Player
+{
+    
+}
